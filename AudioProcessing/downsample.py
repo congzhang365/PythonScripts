fa63cc26c5b7ac6f83a18a8b604b62c770a22f9d
Cong Zhang <rolin365@gmail.com>
2020-04-20 19:58:50 +0100

    added/edited functions: check_channel_num, reduce_channel, resample

--- AudioProcessing/downsample.py
+++ AudioProcessing/downsample.py
@@ -3,9 +3,20 @@ from parselmouth.praat import call
 import os
 
 
+def check_channel_num(input_dir):
+    if os.path.isdir(input_dir):
+        for root, dirs, files in os.walk(input_dir):
+            break
+
+        for file in files:
+            if file.endswith('.wav'):
+                snd = parselmouth.Sound(input_dir + file)
+                channel_num = call(snd, "Get number of channels")
+                print('%s\'s channel_num is %s!'%(file, channel_num))
+
+
 def reduce_channel(input_dir, output_dir, channel='mono'):
     if os.path.isdir(input_dir):
-        # print(input_dir)
 
         for root, dirs, files in os.walk(input_dir):
             break
@@ -13,14 +24,20 @@ def reduce_channel(input_dir, output_dir, channel='mono'):
             if file.endswith('.wav'):
                 snd = parselmouth.Sound(input_dir + file)
                 if channel is 'mono':
-                    new = call(snd, "Convert to mono")
+                    channel_num = call(snd, "Get number of channels")
+                    if not channel_num == 1:
+                        new = call(snd, "Convert to mono")
+                        print('%s has been converted!'%file)
+                        if os.path.isdir(output_dir):
+                            new.save("%s%s"%(output_dir,file), "WAV")
+                        else:
+                            os.makedirs(os.path.dirname(output_dir))
+                            new.save("%s%s" % (output_dir, file), "WAV")
+                    else:
+                        print('%s has 1 channel!'%file)
                 else:
                     print("Do you want to convert your sound to mono?")
-                if os.path.isdir(output_dir):
-                    new.save("%s%s"%(output_dir,file), "WAV")
-                else:
-                    os.makedirs(os.path.dirname(output_dir))
-                    new.save("%s%s" % (output_dir, file), "WAV")
+
 
 
 def resample(input_dir, output_dir, out_rate=8000):
@@ -41,10 +58,14 @@ def resample(input_dir, output_dir, out_rate=8000):
                     os.makedirs(os.path.dirname(output_dir))
                     resampled.save("%s%s" % (output_dir, file), "WAV")
 
+
 if __name__ == "__main__":
-    path_in = "C:\cygwinfolders\cppfa\wav8k/" #  add asterisks is for looping through subdir
-    path_out = "D:\Rokid\Projects\Songs/annotation\LH\wav8k/"
-    reduce_channel(path_in,path_in)
-    # resample(path_in, path_out, out_rate=8000)
+    path_in = "C:/Users/audio/" #  add an asterisk for looping through subdir
+    path_out = "C:/Users/audio/"
 
-
+    # check the number of channels
+    check_channel_num(path_in)
+    # reduce to mono
+    reduce_channel(path_in, path_out)
+    # resample audio
+    resample(path_in, path_out, out_rate=8000)
